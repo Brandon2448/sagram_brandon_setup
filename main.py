39cd1004df85921f2ea009bdff518cc5867b01d3
Brandon2448 <72761767+Brandon2448@users.noreply.github.com>
2020-12-22 09:35:34 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 while True:
-    light.show_animation(light.rainbowAnimation, 500)
+    print("Light Level = " + (input.light_level()))
+    if input.light_level() > 5:
+        light.set_all(light.rgb(0, 0, 0))
+    elif 6 > input.light_level() > 1:
+        light.set_all(light.rgb(0, 0, 255))
+    else:
+        light.set_all(light.rgb(255, 255, 0))
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 while (true) {
-    light.showAnimation(light.rainbowAnimation, 500)
+    console.log("Light Level = " + input.lightLevel())
+    if (input.lightLevel() > 5) {
+        light.setAll(light.rgb(0, 0, 0))
+    } else if (6 > input.lightLevel() && input.lightLevel() > 1) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else {
+        light.setAll(light.rgb(255, 255, 0))
+    }
+    
 }
