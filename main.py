1ea59760aaed1a2a8792063a6f57df84627014f5
Brandon2448 <72761767+Brandon2448@users.noreply.github.com>
2021-01-14 13:17:59 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("Light Level = " + (input.light_level()))
-    if input.light_level() > 20:
+    if input.light_level() > 10:
         music.pew_pew.play()
     else:
         light.set_all(light.rgb(255, 255, 255))
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("Light Level = " + input.lightLevel())
-    if (input.lightLevel() > 20) {
+    if (input.lightLevel() > 10) {
         music.pewPew.play()
     } else {
         light.setAll(light.rgb(255, 255, 255))
