16f26b27aa0e4637d46440ade623ecf28a98857d
Brandon2448 <72761767+Brandon2448@users.noreply.github.com>
2021-01-19 11:09:37 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# while True:
+ #   if input.light_level() < 5 and input.sound_level() > 540:
+     #       music.siren.play_until_done()
+     #   light.set_all(color.rgb(255, 0, 0))
+     #   pause(30000)
+     #    else:
+        #        light.clear()
+
 while True:
-    if input.light_level() < 5 and input.sound_level() > 540:
-        music.siren.play_until_done()
-        light.set_all(color.rgb(255, 0, 0))
-        pause(30000)
-    else:
-        light.clear()
-        
+    music.play_melody("E B E B E B G G A5 C5 C A A5 C5 C A", 200)
