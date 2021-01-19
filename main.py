0db092950626c7b5e0bdc3588546a77d5eeb7bf6
Brandon2448 <72761767+Brandon2448@users.noreply.github.com>
2021-01-19 10:28:46 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 while True:
-    print("Light Level = " + (input.light_level()))
-    if input.light_level() > 30:
-        music.pew_pew.play_until_done()
+    if input.light_level() < 5 and input.sound_level() > 540:
+        music.siren.play_until_done()
+        light.set_all(color.rgb(255, 0, 0))
+        pause(30000)
     else:
         light.clear()
         
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 while (true) {
-    console.log("Light Level = " + input.lightLevel())
-    if (input.lightLevel() > 30) {
-        music.pewPew.playUntilDone()
+    if (input.lightLevel() < 5 && input.soundLevel() > 540) {
+        music.siren.playUntilDone()
+        light.setAll(color.rgb(255, 0, 0))
+        pause(30000)
     } else {
         light.clear()
     }
